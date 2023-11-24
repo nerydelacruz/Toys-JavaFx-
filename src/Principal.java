03a65666cadcc8d62acf5353a90ce11dcba710ec
Nery de la Cruz <nerydelacruzhuinil@gmail.com>
2023-11-23 20:02:03 -0600

    Se agregaron las librerias de JavaFx y se implemento el Application

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.sql.SQLOutput;
 
-public class Principal {
+public class Principal extends Application {
 
     public static void main(String [] args){
         System.out.println("Hola");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }

5d7dfc3485127c38c2eeb6911fdb86afe4f8e6c7
Nery de la Cruz <nerydelacruzhuinil@gmail.com>
2023-11-24 13:53:14 -0600

    Estructura del Controlador
    Please enter the commit message for your changes. Lines starting

--- src/org/nerydelacruz/controller/ControladorPrincipal.java
+++ src/org/nerydelacruz/controller/ControladorPrincipal.java
@@ -1,4 +1,13 @@
 package org.nerydelacruz.controller;
 
-public class ControladorPrincipal {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorPrincipal implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }

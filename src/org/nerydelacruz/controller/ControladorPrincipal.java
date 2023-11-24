76ac4fd55a6a97c115224db07bd7224bca891648
Nery de la Cruz <nerydelacruzhuinil@gmail.com>
2023-11-24 14:02:48 -0600

    Se termino de completar el Controlador de la Clase Principal

--- src/org/nerydelacruz/controller/ControladorPrincipal.java
+++ src/org/nerydelacruz/controller/ControladorPrincipal.java
@@ -1,13 +1,23 @@
 package org.nerydelacruz.controller;
 
 import javafx.fxml.Initializable;
+import org.nerydelacruz.sistema.Principal;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControladorPrincipal implements Initializable {
+    private Principal escenarioPrincipal;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
+    }
+
+    public Principal getEscenarioPrincipal() {
+        return escenarioPrincipal;
+    }
+
+    public void setEscenarioPrincipal(Principal escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
     }
 }
